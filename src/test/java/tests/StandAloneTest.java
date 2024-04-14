ff082252fed4b4fb2d511ef0fb0f52282cdfd0b9
ronit <ronitpadgaonkar@gmail.com>
2024-04-14 18:49:57 +0530

    Standalone Changes

--- src/test/java/tests/StandAloneTest.java
+++ src/test/java/tests/StandAloneTest.java
@@ -17,7 +17,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class StandAloneTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
